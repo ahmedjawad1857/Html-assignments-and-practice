6f644dc0cd9aea774086a9cdfa3576a7f4ec1a5e
Ahmad Jawad <134083952+ahmedjawad1857@users.noreply.github.com>
2024-06-04 19:22:10 +0500

    take input from user in html,css,typescript

---
+++ how can we take info from user in input field/index.js
@@ -0,0 +1,13 @@
+// // write a ts program to display sum of 1-100 all even numbers and show the numbers which numbers sum is this
+// var sum: number = 0;
+// var list: number[] = [];
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     sum += i;
+//     list.push(i);
+//   }
+// }
+// console.log(`sum:${sum} \nnumber which sum:${list}`);
+if (12 == 12) {
+  console.log("jepoqjsoi ");
+}
